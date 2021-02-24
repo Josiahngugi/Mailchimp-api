b235535a04fb77c7212007482f770ccf19648e44
josiahngugi <mainajosiahdev@gmail.com>
2021-02-25 01:32:14 +0800

    adding a public folder to take styles and images online

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("hey");
+  res.sendFile(__dirname + "/signup.html");
 
 })
 
