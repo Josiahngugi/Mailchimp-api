5c49ca8a604fb8c1b1f20eedabf17e4f4acec12e
josiahngugi <mainajosiahdev@gmail.com>
2021-02-25 01:16:28 +0800

    adding sign up page

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.send("hey");
+
+})
+
+app.listen(3000,function(){
+  console.log("server is running on port 3000");
+})
